5e969e067210def3213125d6b41e89713648f1e7
Benji <benji@benjisoft.org.uk>
2018-12-11 16:27:47 +0000

    Another day

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-choice = str.lower(input("What would you like to do? (Play/ Quit)"))
-if choice == "play":
-    print("Welcome!")
-else:
-    print('BoobS')
+from random import randint
+from linereader import getline
+
+randline = getline("dogs.txt", randint(1,30))
+
+while 1: 
+    choice = str.lower(input("What would you like to do? (Play/ Quit)"))
+    if choice == "play":
+        print("Welcome!")
+        print(randline)
+    elif choice=="quit":
+        print("")
+        break
+    else:
+        print('Unrecognised Input, Damnit!')
