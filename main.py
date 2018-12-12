efd6b491406331ebf8085e233eb8c2e54d650e3f
Benji <benji@benjisoft.org.uk>
2018-12-12 10:45:32 +0000

    Sentiment analysis with AWS

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from random import randint
 from linereader import getline
+import boto3
+import json
 
+comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1')
 randline = getline("dogs.txt", randint(1,30))
 
 while 1: 
     choice = str.lower(input("What would you like to do? (Play/ Quit)"))
+    print('Calling DetectSentiment')
+    result = comprehend.detect_sentiment(Text=choice, LanguageCode='en') #, sort_keys=True, indent=4)
+    positiveness = result["SentimentScore"]["Positive"]
+    print(positiveness)
+    print('End of DetectSentiment\n')
+
     if choice == "play":
         print("Welcome!")
         print(randline)
