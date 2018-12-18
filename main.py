65e96f9cdd74496fd4f01136d693828ab9a76840
Benji <benji@benjisoft.org.uk>
2018-12-18 16:29:37 +0000

    Todays work

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 from random import randint
 from linereader import getline
+from time import sleep
 import boto3
 import json
 
 comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1')
-randline = getline("dogs.txt", randint(1,30))
+# Change variable production to 0 for in order to enable verbose mode. 
+production = 1
+if production == 1: 
+    v = input("Would you like to enable verbose mode? (0/1)")
+else:
+    v=0
 
 while 1: 
     choice = str.lower(input("What would you like to do? (Play/ Quit)"))
-    print('Calling DetectSentiment')
-    result = comprehend.detect_sentiment(Text=choice, LanguageCode='en') #, sort_keys=True, indent=4)
-    positiveness = result["SentimentScore"]["Positive"]
-    print(positiveness)
-    print('End of DetectSentiment\n')
-
-    if choice == "play":
-        print("Welcome!")
-        print(randline)
-    elif choice=="quit":
-        print("")
+    if v == 1:
+        print('Calling DetectSentiment')
+    if choice != "":
+        result = comprehend.detect_sentiment(Text=choice, LanguageCode='en') #, sort_keys=True, indent=4)
+        p = result["SentimentScore"]["Positive"]
+        if v == 1:
+            print("P = ", p)
+        n = result["SentimentScore"]["Negative"]
+        if v == 1:
+            print("N = ", n)
+    else: 
+        print("No input detected, quitting...")
+        sleep(1)
         break
+    if p>=n:
+        randline = getline("dogs.txt", randint(1,30))
+        excersise = randint(1,5)
+        inteligence = randint(1,100)
+        friendliness = randint(1,10)
+        drool = randint(1,10)
+        print("Welcome!")
+        print(excersise)
+        print(randline)
     else:
-        print('Unrecognised Input, Damnit!')
+        print("Exiting...")
+        break
